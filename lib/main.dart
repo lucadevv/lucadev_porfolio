c03b2d0fdd0dc5b36100cc40fc38aa5ff2512022
lucadevv <luiscodecarranza@gmail.com>
2024-06-05 20:23:12 -0500

    url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lucadev_porforlio/main/main_app.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
   runApp(const MainApp());
 }
